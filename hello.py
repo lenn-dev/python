cba4846fc47b245c22978e5dbc1bca7bfe891fed
soo <adellakwon@gmail.com>
2024-02-19 21:05:13 +1300

    no message

---
+++ hello.py
@@ -0,0 +1,24 @@
+# 실행하기 ctrl+option+N
+print("Hello world")
+
+# variables
+# 파이썬은 코드를 위 => 아래로 순차적으로 읽는다.
+a = 2
+b = 3
+c = a + b
+c = 1
+a = 4
+b = 5
+print(c)
+print(a)
+print(b)
+
+# data type : number, text, boolean
+# True/False 대문자로 꼭 쓰기
+my_name ="len"
+age = 12
+dead = False
+print("Hi my name is ",my_name)
+print("and I'm",age, "years old")
+
+
